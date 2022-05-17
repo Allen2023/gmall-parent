40d9a6854783dbed3518c65d19aedb4d26cd66b0
XSZ <1273343014@qq.com>
2022-05-17 22:36:03 +0800

    第一次提交

--- service/service-product/src/main/java/com/atguigu/gmall/product/mapper/CategoryMapper2.java
+++ service/service-product/src/main/java/com/atguigu/gmall/product/mapper/CategoryMapper2.java
@@ -1,7 +1,7 @@
 package com.atguigu.gmall.product.mapper;
 import com.atguigu.gmall.model.product.BaseCategory2;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import org.springframework.stereotype.Repository;
+
 
 
 public interface CategoryMapper2  extends BaseMapper<BaseCategory2> {
