46aef0c5b9bd512edf1d27e3371dff81239e47d0
XSZ <1273343014@qq.com>
2022-05-18 13:44:12 +0800

    第一次提交

--- service/service-product/src/main/java/com/atguigu/gmall/product/ProductApplication.java
+++ service/service-product/src/main/java/com/atguigu/gmall/product/ProductApplication.java
@@ -1,12 +1,16 @@
 package com.atguigu.gmall.product;
 
+import com.atguigu.gmall.common.config.MybatisPlusConfig;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.cloud.client.SpringCloudApplication;
 
+import org.springframework.context.annotation.Import;
+
 
 
 @MapperScan(basePackages = "com.atguigu.gmall.product.mapper")
+@Import(MybatisPlusConfig.class)
 @SpringCloudApplication
 public class ProductApplication {
     public static void main(String[] args) {
