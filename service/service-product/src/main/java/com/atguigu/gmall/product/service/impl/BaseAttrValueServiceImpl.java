3d60d0af07682a8d6fc734f36279c53d6d166cec
XSZ <1273343014@qq.com>
2022-05-18 19:03:26 +0800

    第一次提交

---
+++ service/service-product/src/main/java/com/atguigu/gmall/product/service/impl/BaseAttrValueServiceImpl.java
@@ -0,0 +1,23 @@
+package com.atguigu.gmall.product.service.impl;
+
+import com.atguigu.gmall.model.product.BaseAttrValue;
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+
+import com.atguigu.gmall.product.service.BaseAttrValueService;
+import com.atguigu.gmall.product.mapper.BaseAttrValueMapper;
+import org.springframework.stereotype.Service;
+
+/**
+* @author 86185
+* @description 针对表【base_attr_value(属性值表)】的数据库操作Service实现
+* @createDate 2022-05-18 18:17:03
+*/
+@Service
+public class BaseAttrValueServiceImpl extends ServiceImpl<BaseAttrValueMapper, BaseAttrValue>
+    implements BaseAttrValueService{
+
+}
+
+
+
+
