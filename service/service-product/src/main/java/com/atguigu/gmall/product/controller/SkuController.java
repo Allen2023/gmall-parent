e561513a718226385b2ca6017b212f7b66a98b46
XSZ <1273343014@qq.com>
2022-05-20 14:53:46 +0800

    Spu接口+Sku接口

--- service/service-product/src/main/java/com/atguigu/gmall/product/controller/SkuController.java
+++ service/service-product/src/main/java/com/atguigu/gmall/product/controller/SkuController.java
@@ -9,7 +9,6 @@ import com.atguigu.gmall.product.service.SpuImageService;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
-import org.checkerframework.checker.units.qual.A;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
