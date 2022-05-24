f0c5c396de091d0cd6b80ac8412c0ceb496f9a5d
XSZ <1273343014@qq.com>
2022-05-25 00:18:51 +0800

    Redisson布隆过滤器小测试

--- service/service-product/src/test/java/com/atguigu/gmall/product/RedisTest.java
+++ service/service-product/src/test/java/com/atguigu/gmall/product/RedisTest.java
@@ -1,6 +1,8 @@
 package com.atguigu.gmall.product;
 
 import org.junit.jupiter.api.Test;
+import org.redisson.api.RBloomFilter;
+import org.redisson.api.RedissonClient;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.redis.core.StringRedisTemplate;
@@ -13,14 +15,45 @@ import org.springframework.data.redis.core.StringRedisTemplate;
 //@SpringBootTest
 public class RedisTest {
 
-   /* @Autowired
-    StringRedisTemplate stringRedisTemplate;
+    /* @Autowired
+     StringRedisTemplate stringRedisTemplate;
+
+
+     //@Test
+     void redisTest(){
+         stringRedisTemplate.opsForValue().set("hello", "world");
+         String hello = stringRedisTemplate.opsForValue().get("hello");
+         System.out.println("hello = " + hello);
+
+     }*/
+    @Autowired
+    RedissonClient redissonClient;
 
     //@Test
-    void redisTest(){
-        stringRedisTemplate.opsForValue().set("hello", "world");
-        String hello = stringRedisTemplate.opsForValue().get("hello");
-        System.out.println("hello = " + hello);
+    void redissonTest() {
+        //1.获取布隆过滤器
+        RBloomFilter<Object> bloomFilter = redissonClient.getBloomFilter("sku:bloom");
+        //2.初始化布隆
+        // long expectedInsertions  预期的插入值
+        //double falseProbability  错误概率
+        bloomFilter.tryInit(1000000, 0.00001);
 
-    }*/
+        //保存数据
+        bloomFilter.add(11L);
+        bloomFilter.add(12L);
+        bloomFilter.add(13L);
+
+        //判断有无数据
+        System.out.println("bloomFilter.contains(11L) = " + bloomFilter.contains(11L));
+        System.out.println("bloomFilter.contains(12L) = " + bloomFilter.contains(12L));
+        System.out.println("bloomFilter.contains(13L) = " + bloomFilter.contains(13L));
+
+
+    }
+    //@Test
+    void aaa(){
+        System.out.println("redissonClient.getBloomFilter(\"sku:bloom\") = " + redissonClient.getBloomFilter("sku:bloom"));
+
+    }
+
 }
