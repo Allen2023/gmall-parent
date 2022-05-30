b30c84f85267e65c19dcd1fe7444e0c5fdb5eec5
XSZ <1273343014@qq.com>
2022-05-31 01:16:56 +0800

    检索商品

--- service/service-item/src/main/java/com/atguigu/gmall/item/config/AppConfig.java
+++ service/service-item/src/main/java/com/atguigu/gmall/item/config/AppConfig.java
@@ -2,9 +2,6 @@ package com.atguigu.gmall.item.config;
 
 
 import com.atguigu.gmall.common.annotation.EnableAppThreadPool;
-
-import com.atguigu.gmall.starter.cache.aop.annotation.EnableAutoCache;
-import com.atguigu.gmall.starter.cache.aop.annotation.EnableRedissonandCache;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
@@ -13,7 +10,7 @@ import org.springframework.context.annotation.EnableAspectJAutoProxy;
 //每个微服务中，自己需要调用谁就导入谁
 @EnableAspectJAutoProxy//开启切面自动代理
 @EnableAppThreadPool//开启自定义线程池配置
-@EnableFeignClients(basePackages = "com.atguigu.gmall.feign.product")
+@EnableFeignClients(basePackages = {"com.atguigu.gmall.feign.product","com.atguigu.gmall.feign.list"})
 @Configuration
 public class AppConfig {
 
